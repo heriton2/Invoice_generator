de378ac41699474189e12812301d68aff1c0ec0f
Heriton <heriton2@gmail.com>
2023-06-19 17:00:56 -0300

    Implementação de testes unitários.

--- src/main/java/com/challenge/invoice_generator/repository/ImportedItemRepository.java
+++ src/main/java/com/challenge/invoice_generator/repository/ImportedItemRepository.java
@@ -1,7 +1,6 @@
 package com.challenge.invoice_generator.repository;
 
 import com.challenge.invoice_generator.entity.ImportedItem;
-import com.challenge.invoice_generator.entity.InvoiceItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImportedItemRepository extends JpaRepository<ImportedItem, Long> {
