a674ea67740d28358a1ed35841e24f16c4434d82
Heriton <heriton2@gmail.com>
2023-06-23 12:35:44 -0300

    Aceitar origens diferente de localhost do frontend.

--- src/main/java/com/challenge/invoice_generator/config/CorsConfig.java
+++ src/main/java/com/challenge/invoice_generator/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Adicione a origem permitida para o seu frontend
+                .allowedOrigins("*") // Adicione a origem permitida para o seu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Adicione os métodos HTTP permitidos
                 .allowedHeaders("*"); // Permitir todos os cabeçalhos
     }
